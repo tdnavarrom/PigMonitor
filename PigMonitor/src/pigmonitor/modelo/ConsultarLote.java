b821f58e86886a5300bc433b48bcd73dfabc4006
Tomas Navarro <tdnavarrom@eafit.edu.co>
2019-10-15 16:40:22 -0500

    Creacion de archivos del Lote

---
+++ PigMonitor/src/pigmonitor/modelo/ConsultarLote.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pigmonitor.modelo;
+
+/**
+ *
+ * @author tdnavarrom
+ */
+public class ConsultarLote {
+    
+}
