cfb20f941e55972a976c5e4957341e00a06d091e
Tomas Navarro <tdnavarrom@eafit.edu.co>
2019-11-01 17:45:07 -0500

    ahora si

--- PigMonitor/src/pigmonitor/modelo/Conexion.java
+++ PigMonitor/src/pigmonitor/modelo/Conexion.java
@@ -21,21 +21,17 @@ import javax.swing.JOptionPane;
 public class Conexion {
     
  
-    private final String base = "pig_monitor";
-    private final String username = "root";
-    private final String password = "TomasDavid2001";
-    private final String url = "jdbc:mysql://localhost:3306/" + base+"?autoReconnect=true&useSSL=false";
+    private final String base = "pigmonitor";
+    private final String username = "pcorream@pigmonitor-server";
+    private final String password = "pitufo2013#";
+    private final String url = "pigmonitor-server.mysql.database.azure.com";
     private Connection con = null;
     
     public Connection getConexion(){
        
        try{
            
-<<<<<<< HEAD
            Class.forName("com.mysql.cj.jdbc.Driver");
-=======
-           Class.forName("com.mysql.jdbc");
->>>>>>> 6e4af69499de4e087667ae8be9fd99f8ef2abe96
            con = (Connection) DriverManager.getConnection(this.url, this.username, this.password);
            
        }catch(SQLException e){
